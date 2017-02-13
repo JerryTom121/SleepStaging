1254fe3a8eeead0236bafe3c2b70944579cbbe03
Djordje <miladinovicdjordje@yahoo.com>
2017-02-13 14:54:58 +0100

    asd

--- deepnet/predict.sh
+++ deepnet/predict.sh
@@ -1,2 +1,6 @@
 rm -f /home/sleep/data/results/artifacts.txt
 CUDA_VISIBLE_DEVICES=0 th NNpredict.lua
+# echo "It should have the filename"
+# echo $1
+# CUDA_VISIBLE_DEVICES=0 th NNpredict.lua $1
+# echo "Done with Djordje's predictions"
